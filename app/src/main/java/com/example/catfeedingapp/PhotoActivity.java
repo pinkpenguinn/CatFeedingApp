58f197b4fd743a639e87be3ae2e200a7480039f3
Janani <jananitamilselvan3@gmail.com>
2020-06-15 19:24:43 +0800

    test

--- app/src/main/java/com/example/catfeedingapp/PhotoActivity.java
+++ app/src/main/java/com/example/catfeedingapp/PhotoActivity.java
@@ -2,7 +2,13 @@ package com.example.catfeedingapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Bundle;
+
 public class PhotoActivity extends AppCompatActivity {
 
-
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_photo2);
+    }
 }
